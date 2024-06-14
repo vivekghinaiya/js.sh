612c8d81e3b0c74919519b494175d4b4ee8dbf87
Vivek ghinaiya <56452603+vivekghinaiya@users.noreply.github.com>
2024-06-14 10:53:44 +0530

    Update js.sh
    sda

--- js.sh
+++ js.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-domain=$1
-echo "$domain" | gau | grep ".js" | tee jslinks.txt
-echo "$domain" | waybackurls | grep ".js" | tee -a jslinks.txt
-echo "$domain" | subjs | tee -a jslinks.txt
+cat live.txt | gau | grep ".js" | tee jslinks.txt
+cat live.txt | waybackurls | grep ".js" | tee -a jslinks.txt
+cat live.txt | subjs | tee -a jslinks.txt
+cat live.txt | katana -jc | grep ".js" | tee -a jslinks.txt
+waymore -i live.txt -mode U | grep ".js" | tee -a jslinks.txt
 cat jslinks.txt | uniq | tee jslinks.txt
 
 cat jslinks.txt | httpx | jslive.txt
+
+echo "= parsing the js file through nuclei ="
 nuclei -l jslinks.txt -t /root/nuclei-templates/http/exposures/ -o js_bugs.txt
+
+echo "= parsing the js file through secretfinder ="
+cat jslinks.txt  | while read url; do python3 /home/tools/secretfinder/SecretFinder.py -i $url -o cli; done
